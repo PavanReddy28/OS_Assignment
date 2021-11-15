ecfb1e9a29bf4aa65bf38aad9ab917e795a825ce
PavanReddy28 <ypavan2802@gmail.com>
2021-11-15 22:29:14 +0530

    Final Version without analysis

--- gen_num.c
+++ gen_num.c
@@ -1,40 +1,28 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-
-int main() {
-    
+int main()
+{
 
     FILE *p;
     p = fopen("./n2.txt", "a+");
-
     int num = 1000100;
-    //fprintf(p, "%d", 123);
-    for (int i=0;i<num;i++) {
-        //fscanf(p, "%d\n", &n);
-        //putw(rand(), p);
-        fprintf(p, "%d\n", rand()%1000000);
-        //i = getw(p);
-        // printf("%d\n", n);
+    for (int i = 0; i < num; i++)
+    {
+        fprintf(p, "%d\n", rand() % 1000000);
     }
     fclose(p);
 
     FILE *f;
     f = fopen("./n3.txt", "a+");
-
-    // int num = 1000100;
-    //fprintf(p, "%d", 123);
-    for (int i=0;i<num;i++) {
-        //fscanf(p, "%d\n", &n);
-        //putw(rand(), p);
-        fprintf(f, "%d\n", rand()%1000000);
-        //i = getw(p);
-        // printf("%d\n", n);
+    for (int i = 0; i < num; i++)
+    {
+        fprintf(f, "%d\n", rand() % 1000000);
     }
     fclose(f);
     return 0;
